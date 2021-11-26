537d0fbc394f303844fc864ee857d231ad24d126
AndNasPlay <shchekatunov.av@gmail.com>
2021-11-26 17:12:47 +0300

    project created

---
+++ CompositeAndChainOfResponsibility/CompositeAndChainOfResponsibility/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  CompositeAndChainOfResponsibility
+//
+//  Created by Андрей Щекатунов on 26.11.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .white
+		self.navigationController?.navigationBar.isHidden = true
+	}
+}
